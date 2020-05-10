3c6e07b4d7ec42b1785087838b32ed51bfcdfccd
Narendra Reddy <narendra@Narendras-MacBook-Pro.local>
2020-05-10 17:58:18 +0530

    Initial Commit

---
+++ SampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleApp
+//
+//  Created by Narendra Reddy on 5/10/20.
+//  Copyright © 2020 Narendra Reddy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
