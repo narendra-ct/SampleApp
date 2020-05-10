4b05ca7b1c5dbef6cc970042d4e17eaae0e05690
Narendra Reddy <narendra@Narendras-MacBook-Pro.local>
2020-05-10 18:15:53 +0530

    Test

--- SampleApp/ContentView.swift
+++ SampleApp/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World! - Welcome to CI/CD")
+        Text("Hello, World! - Welcome to CI/CD     :: Welcome ::")
     }
 }
 
